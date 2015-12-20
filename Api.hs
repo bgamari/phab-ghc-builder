72fb2372a0e6355460cc0890695bbb4e4959f0a4
Ben Gamari <ben@smart-cactus.org>
2015-12-21 00:39:59 +0100

    Cleanup

--- Api.hs
+++ Api.hs
@@ -5,13 +5,13 @@
 
 module Api where
 
-import Data.Aeson
 import Servant
-import Data.Text (Text)
 
 import Build
 import Harbormaster
 
+-- | Build a differential.
+-- @http://host/build/diff?id=${build.id}&rev=${buildable.revision}&diff=${buildable.diff}&base_commit=${buildable.commit}&phid=${target.phid}@
 type BuildDiff = "build" :> "diff"
                  :> QueryParam "id" BuildId
                  :> QueryParam "rev" Revision
@@ -20,12 +20,15 @@ type BuildDiff = "build" :> "diff"
                  :> QueryParam "phid" Phid
                  :> Post '[JSON] ()
 
+-- | Build a commit.
+-- @http://host/build/commit?id=${build.id}&commit=${buildable.commit}&phid=${target.phid}@
 type BuildCommit = "build" :> "commit"
                    :> QueryParam "id" BuildId
                    :> QueryParam "commit" Commit
                    :> QueryParam "phid" Phid
                    :> Post '[JSON] ()
 
+-- | The build server API
 type Api = BuildDiff :<|> BuildCommit
 
 api :: Proxy Api
