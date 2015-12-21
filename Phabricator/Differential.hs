466960b771742dc231b566efbf534348601f740e
Ben Gamari <ben@smart-cactus.org>
2015-12-21 03:24:21 +0100

    Add Phabricator.Differential

---
+++ Phabricator/Differential.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Phabricator.Differential
+    ( -- * Differential API
+      createComment
+      -- * Messages
+    , Comment(..)
+    ) where
+
+import Control.Monad.Trans.Either
+import qualified Data.Text as T
+
+import Servant
+import Servant.Client
+import Phabricator.Types
+
+data Comment = Comment { cmtBody :: Maybe T.Text
+                       , cmtAction :: Maybe T.Text -- TODO
+                       , cmtSilent :: Bool
+                       }
+
+createComment :: BaseUrl -> ApiToken -> Revision -> Comment -> EitherT ServantError IO ()
+createComment baseUrl apiToken rev cmt =
+    client api baseUrl (Just apiToken) (Just rev)
+                                       (cmtBody cmt)
+                                       (cmtAction cmt)
+                                       (cmtSilent cmt)
+  where
+    api :: Proxy CreateComment
+    api = Proxy
+
+-- | The @differential.createcomment@ endpoint.
+type CreateComment = "api" :> "differential.createcomment"
+                     :> QueryParam "api.token" ApiToken
+                     :> QueryParam "revision_id" Revision
+                     :> QueryParam "message" T.Text
+                     :> QueryParam "action" T.Text
+                     :> QueryFlag "silent"
+                     :> Get '[JSON] ()
